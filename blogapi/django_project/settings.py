8547ff5e9e6e01742dac19dd118d988860c1ca8a
Sasa Buklijas <buklijas.sasa@gmail.com>
2024-03-26 17:56:45 +0100

    posts added

--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Local 
-    'accounts.apps.AccountsConfig', # new
+    # Local
+    'accounts.apps.AccountsConfig',
+    "posts.apps.PostsConfig",
 ]
 
 MIDDLEWARE = [
